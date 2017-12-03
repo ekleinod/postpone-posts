30dbe3edb17c1be2a329b1eac8dbad6c7aa81a0d
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-12-03 18:29:45 +0100

    license and description

---
+++ postpone-posts.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Postpone Posts
+Plugin URI; http://www.ekkart.de/computer/wordpress/plugins.html#postpone-posts
+Description: Postpones all future (planned) posts by a selectable number of days.
+Version: 0.1.0
+Author: Ekkart Kleinod
+Author URI: http://www.edgesoft.de/
+License: GPL-3.0
+License URI: https://opensource.org/licenses/GPL-3.0
+
+Postpone Posts is a wordpress plugin that postpones all future (planned) posts by a selectable number of days.
+Copyright (C) 2017 Ekkart Kleinod
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
